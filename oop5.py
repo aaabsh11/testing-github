7e1066c15ad03a104ece6790ba4c8e98eed3bd32
Абишев Айдар Аскарович <AbishevAy@lcoast.itstep>
2024-01-24 20:50:30 +0600

    initial commit

---
+++ oop5.py
@@ -0,0 +1,18 @@
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __add__(self, p: Point):
+
+#         return
+
+# def __eq__(self, p: Point):
+
+
+def length(self):
+    self.length = len(self)
+    return self.length
+
+
+length(26)
